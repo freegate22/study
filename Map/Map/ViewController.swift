7236cac8f990f50feebb3f02fcb8a3d0c4438111
na <freegate22@hotmail.com>
2016-10-24 13:40:01 +0900

    Map first commit

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by na on 2016. 10. 24..
+//  Copyright © 2016년 na. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
