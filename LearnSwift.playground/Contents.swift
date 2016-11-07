30f848f30426b4451cb4c882b4a8f8026cfdaff5
HSJ <freegate22@hotmail.com>
2016-11-07 20:27:09 +0900

    IOS9 - Interface

---
+++ LearnSwift.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("Welcome")
+
+var x = 10
+
+for index in 1...20{
+    x = x - 1
+    let y = index * x
+}
+
+/*:
+ # Welcome to Playgrounds
+ This is your *first* playground which is intented to demonstrate:
+ * The use of **Quck Look**
+ * Placing results **in-line** with the code
+ */
+
---
+++ LearnSwift.playground/Pages/UIKit Examples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: [Previous](@previous)
+
+//import Foundation
+import UIKit
+import PlaygroundSupport
+
+var index: Int?
+
+index = 3
+if index != nil {
+    print(index!)
+} else {
+    print("b")
+}
+
+
+if index != nil {
+    print(index!)
+} else {
+    print("b")
+}
+
+/*
+let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+myLabel.backgroundColor = UIColor.red
+myLabel.text = "Hello Swift"
+myLabel.textAlignment = .center
+myLabel.font = UIFont(name: "Gergia", size: 24)
+
+let image = UIImage(named: "waterfall.png")
+
+let container = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+
+PlaygroundPage.current.liveView = container
+
+container.backgroundColor = UIColor.white
+let square = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+square.backgroundColor = UIColor.red
+
+container.addSubview(square)
+
+UIView.animate(withDuration: 5.0, animations: {
+    square.backgroundColor = UIColor.blue
+    let rotation = CGAffineTransform(rotationAngle: 3.14)
+    square.transform = rotation
+})
+*/
+
+//: [Next](@next)
+
+
