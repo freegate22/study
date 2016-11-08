1ddba973e5d19812688973e9b8f22636b040bf9e
HSJ <freegate22@hotmail.com>
2016-11-08 12:49:21 +0900

    IOS9 - 함수 클로저

--- LearnSwift.playground/Pages/UIKit Examples.xcplaygroundpage/Contents.swift
+++ LearnSwift.playground/Pages/UIKit Examples.xcplaygroundpage/Contents.swift
@@ -20,7 +20,6 @@ if index != nil {
     print("b")
 }
 
-/*
 let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
 myLabel.backgroundColor = UIColor.red
 myLabel.text = "Hello Swift"
@@ -44,7 +43,99 @@ UIView.animate(withDuration: 5.0, animations: {
     let rotation = CGAffineTransform(rotationAngle: 3.14)
     square.transform = rotation
 })
-*/
+
+
+func message( name: String, count: Int) -> String{
+    var name = name
+    name = name + " !"
+    return("\(name), ye")
+}
+
+
+message(name: "ya",count: 1)
+
+func sizeConverter(length: Float) -> (yards: Float, centimeters: Float, meters: Float){
+    let yards = length * 0.02777778
+    let centimeters = length * 2.54
+    let meters = length * 0.0254
+    
+    return (yards, centimeters, meters)
+}
+
+let lengthTuple = sizeConverter(length: 20)
+
+print(lengthTuple.yards)
+print(lengthTuple.centimeters)
+print(lengthTuple.meters)
+
+
+func displayStrings(strings: String...){
+    for string in strings{
+        print(string)
+    }
+}
+
+displayStrings(strings: "one","two","three")
+
+
+var myValue = 10
+func doubleValue(value: inout Int) -> Int{
+    value += value
+    return(value)
+}
+
+print("Before = \(myValue)")
+print("call = \(doubleValue(value: &myValue))")
+print("After = \(myValue)")
+
+func inchesToFeet(inches: Float) -> Float{
+    return inches * 0.833333
+}
+func inchesToYards(inches: Float) -> Float{
+    return inches * 0.0277778
+}
+
+let toFeet = inchesToFeet
+let toYards = inchesToYards
+let result = toFeet(10)
+
+func outputConversion(converterFunc: (Float) -> Float, value: Float){
+    let result = converterFunc(value)
+    print("result \(result)")
+}
+
+outputConversion(converterFunc: toFeet, value: 10)
+
+
+func decideFunction(feet: Bool) -> (Float) -> Float{
+    if feet {
+        return toFeet
+    } else {
+        return toYards
+    }
+}
+
+let sayHello = { print("Hello") }
+sayHello()
+
+let multiply = {
+    (val1: Int, val2: Int) -> Int in
+    return val1 * val2
+}
+let res = multiply(10, 20)
+
+func funcA() -> () -> Int{
+    var counter = 0
+    
+    func funcB() -> Int{
+        return counter + 10
+    }
+    return funcB
+}
+
+let myClosure = funcA()
+let resultClosure = myClosure()
+
 
 //: [Next](@next)
 
