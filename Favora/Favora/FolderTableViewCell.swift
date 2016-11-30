26aeaab3a022c29f2e7bac16281d145125e201be
HSJ <freegate22@hotmail.com>
2016-11-30 12:22:51 +0900

    Favora

--- Favora/Favora/FolderTableViewCell.swift
+++ Favora/Favora/FolderTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class FolderTableViewCell: UITableViewCell {
 
     @IBOutlet var lblFolderName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Favora/Favora/ViewController.swift
+++ Favora/Favora/ViewController.swift
@@ -12,8 +12,19 @@ var folderNames: [String] = ["포털","블로그","카페","쇼핑몰","기타"]
 
 class ViewController: UIViewController {
 
-    @IBOutlet var folderTableView: UITableView!
+    @IBOutlet var tbvFolder: UITableView!
+    @IBOutlet var edit: UIBarButtonItem!
     
+    @IBAction func doEdit(_ sender: UIBarButtonItem) {
+        if (tbvFolder.isEditing) {
+            edit.title = "Edit"
+            tbvFolder.setEditing(false, animated: true)
+        } else {
+            edit.title = "Done"
+            tbvFolder.setEditing(true, animated: true)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -48,6 +59,45 @@ extension ViewController: UITableViewDataSource{
         // Configure the cell...
         
         return cell
+    }
+    
+    // Override to support editing the table view.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        print("this")
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            folderNames.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "삭제"
+    }
+    
+    // Override to support rearranging the table view.
+    func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        
+        let itemToMove = folderNames[fromIndexPath.row]
+        
+        folderNames.remove(at: fromIndexPath.row)
+        folderNames.insert(itemToMove, at: to.row)
+        
+    }
+    
+    func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tbvFolder.setEditing(editing, animated: animated)
+        if (editing) {
+            // you might disable other widgets here... (optional)
+            print("editing2")
+        } else {
+            // re-enable disabled widgets (optional)
+            print("not editing")
+        }
     }
 }
 
